4e6512d63d5e8a45c83616a87abe52cfbf51d207
Charles Leifer <coleifer@gmail.com>
2015-12-18 19:15:53 -0600

    0.1.1

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ sophy = Extension(
 
 setup(
     name='sophy',
-    version='0.1.0',
+    version='0.1.1',
     description='Python bindings for the sophia database.',
     author='Charles Leifer',
     author_email='',
