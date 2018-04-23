32f8c8438f89fb705f2b9a2a53c2f193fac1322d
Charles Leifer <coleifer@gmail.com>
2018-04-22 23:10:41 -0500

    Version bamp in docs.

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ author = 'Charles Leifer'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '0.4.1'
+release = '0.4.2'
 
 
 # -- General configuration ---------------------------------------------------
